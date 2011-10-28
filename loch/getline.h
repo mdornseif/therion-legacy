421c9a252a02bc05c8cac414163bc5661c21c661
Maximillian Dornseif <md@hudora.de>
2011-10-28 12:59:02 +0200

    compile on MacOS X 10..7.2

--- loch/getline.h
+++ loch/getline.h
@@ -23,10 +23,10 @@ extern "C" {
 
 # include <stdio.h>
 
-int
+ssize_t
 getline (char **_lineptr, size_t *_n, FILE *_stream);
 
-int
+ssize_t
 getdelim (char **_lineptr, size_t *_n, int _delimiter, FILE *_stream);
 
 #ifdef __cplusplus
