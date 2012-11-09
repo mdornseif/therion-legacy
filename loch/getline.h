aed8c3091b5f0fcc4cbb83ebb46d0a98246c7664
Maximillian Dornseif <md@hudora.de>
2012-11-09 21:55:21 +0100

    missed file in last patch

--- loch/getline.h
+++ loch/getline.h
@@ -23,11 +23,13 @@ extern "C" {
 
 # include <stdio.h>
 
+#ifndef __APPLE__
 int
 getline (char **_lineptr, size_t *_n, FILE *_stream);
 
 int
 getdelim (char **_lineptr, size_t *_n, int _delimiter, FILE *_stream);
+#endif
 
 #ifdef __cplusplus
 }
